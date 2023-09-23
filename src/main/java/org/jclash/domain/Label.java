ed93bec1c9a19a4fe6d3a4872ef75d475513d8b5
Giuseppe Valente <valentepeppe@gmail.com>
2023-09-23 23:37:59 +0200

    #9 close

---
+++ src/main/java/org/jclash/domain/Label.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2023 Giuseppe Valente<valentepeppe@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jclash.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Label {
+
+    @JsonProperty
+    private int id;
+
+    @JsonProperty
+    private String name;
+    
+    @JsonProperty
+    private BadgeUrl iconUrls;
+
+    public Label() {
+
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the iconUrls
+     */
+    public BadgeUrl getIconUrls() {
+        return iconUrls;
+    }
+
+    /**
+     * @param iconUrls the iconUrls to set
+     */
+    public void setIconUrls(BadgeUrl iconUrls) {
+        this.iconUrls = iconUrls;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((iconUrls == null) ? 0 : iconUrls.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Label other = (Label) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (iconUrls == null) {
+            if (other.iconUrls != null)
+                return false;
+        } else if (!iconUrls.equals(other.iconUrls))
+            return false;
+        return true;
+    }
+}
